32b84bb90e7416f1e9431aea2353b9cce4fddf48
Bernardo Greco <Bernardo.greco98@gmail.com>
2023-04-12 15:05:24 +0200

    FEAT Table created and datas added to phpmyadmin

---
+++ database/migrations/2023_04_12_115519_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 10);
+            $table->float('price', 5, 2);
+            $table->tinyInteger('carriage')->unsigned();
+            $table->string('on_time', 3)->default('yes');
+            $table->string('canceled', 3)->default('no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
